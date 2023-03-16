50669888063dbfde753cebfd8d31ae89433dfa19
QgQ220903 <111657160+QgQ220903@users.noreply.github.com>
2023-03-17 01:39:25 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let menu_btn = document.querySelector('.menu-btn')
+let navbar_menu = document.querySelector('.navigation')
+const closeMenu = () => {
+  navbar_menu.classList.toggle('navigation_toggle')
+}
+
